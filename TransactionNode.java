1f603d96e5960dbaea1f2a0d5b24ef96debeda8a
christian dees <christianmdees@mgail.com>
2024-11-12 15:26:30 -0700

    added header comment

--- TransactionNode.java
+++ TransactionNode.java
@@ -1,3 +1,13 @@
+/*
+// Authors: Christian Dees & Aitiana Mondragon & Cristina Rivera
+// Date: November 12, 2024
+// Course: CS 3331 - Advanced Object-Oriented Programming - Fall 2024
+// Instructor: Dr. Bhanukiran Gurijala
+// Assignment: Programming Assignment 2 (Project Part 2)
+// Lab Description: This lab is meant to demonstrate our knowledge in object-oriented concepts such as inheritance, polymorphism, UML diagrams, interfaces, design patterns, and more through coding our own implementation of a bank system of which deposits, withdraws, transfer, pays, and generates various files. This lab also included concepts of logging, testing with JUnit, debugging, file reading, error handling and JavaDoc.
+// Honesty Statement: We affirm that we have completed this assignment entirely on our own, without any assistance from outside sources, including peers, experts, online resources, or other means. All code and ideas were that of our own work, and we have followed proper academic integrity.
+ */
+
 /**
  * Transaction Node with its respective attributes for a transaction.
  */
--- UserInterface.java
+++ UserInterface.java
@@ -1,12 +1,12 @@
 /*
-// Authors: Christian Dees & Aitiana Mondragon & Crystina Rivera
-// Date: October 23, 2024
+// Authors: Christian Dees & Aitiana Mondragon & Cristina Rivera
+// Date: November 12, 2024
 // Course: CS 3331 - Advanced Object-Oriented Programming - Fall 2024
 // Instructor: Dr. Bhanukiran Gurijala
-// Assignment: Programming Assignment 1 (Project Part 1)
-// Lab Description: This lab is meant to demonstrate our knowledge in object-oriented concepts such as inheritance, polymorphism, UML diagrams, and more through coding our own implementation of a bank system of which deposits, withdraws, transfer, and pays. This lab also included concepts of logging, testing, debugging, file reading, and JavaDoc.
+// Assignment: Programming Assignment 2 (Project Part 2)
+// Lab Description: This lab is meant to demonstrate our knowledge in object-oriented concepts such as inheritance, polymorphism, UML diagrams, interfaces, design patterns, and more through coding our own implementation of a bank system of which deposits, withdraws, transfer, pays, and generates various files. This lab also included concepts of logging, testing with JUnit, debugging, file reading, error handling and JavaDoc.
 // Honesty Statement: We affirm that we have completed this assignment entirely on our own, without any assistance from outside sources, including peers, experts, online resources, or other means. All code and ideas were that of our own work, and we have followed proper academic integrity.
- */
+*/
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
